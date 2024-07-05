94f9e08c5dc16209707256daed75baa7d2c15dd4
ucok-man <ucokkocu411@gmail.com>
2024-07-05 14:45:12 +0700

    change textarea to markdown ui

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,10 +1,14 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+"use client";
+
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import SimpleMDE from "react-simplemde-editor";
 
 export default function NewIssuePage() {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button className="cursor-pointer">Submit</Button>
     </div>
   );
