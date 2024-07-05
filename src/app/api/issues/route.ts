f40b118e0f7c14a1e5a0ccc69d592fbdf75a8342
ucok-man <ucokkocu411@gmail.com>
2024-07-05 19:52:53 +0700

    add error handling when submit form

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -3,15 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const schema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z
+    .string()
+    .min(1, "title is required")
+    .max(255, "title must be less than 255 char long"),
+  description: z.string().min(2, "description is required"),
 });
 
 export async function POST(req: NextRequest) {
   const input = await req.json();
   const { error, data } = schema.safeParse(input);
   if (error) {
-    return NextResponse.json({ error: error.errors }, { status: 400 });
+    return NextResponse.json({ error: error.format() }, { status: 400 });
   }
   const issue = await prisma.issue.create({
     data: {
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,15 +1,59 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 
+type IssueForm = {
+  title: string;
+  description: string;
+};
+
 export default function NewIssuePage() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [submitError, setSubmitError] = useState<string | null>(null);
+
+  const submitform = async (data: IssueForm) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitError(
+        "Unexpected error has occured. Please check your submission!"
+      );
+    }
+  };
+
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button className="cursor-pointer">Submit</Button>
+      {submitError && (
+        <Callout.Root color="red">
+          <Callout.Text>{submitError}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => submitform(data))}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button className="cursor-pointer">Submit</Button>
+      </form>
     </div>
   );
 }
