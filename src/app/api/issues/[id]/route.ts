40ea10caba63080634ff7db8a042e38f590658c0
ucok-man <ucokkocu411@gmail.com>
2024-07-09 20:42:48 +0700

    add api for delete issue

--- src/app/api/issues/[id]/route.ts
+++ src/app/api/issues/[id]/route.ts
@@ -40,6 +40,40 @@ export async function PATCH(req: NextRequest, { params }: Props) {
 
   const response = { data: updatedIssue };
   return NextResponse.json(response, {
-    status: 201,
+    status: 200,
+  });
+}
+
+export async function DELETE(req: NextRequest, { params }: Props) {
+  const validId = parseInt(params.id);
+  if (!validId)
+    return NextResponse.json(
+      { error: "The id must be number and greater than 0" },
+      { status: 400 }
+    );
+
+  const input = await req.json();
+  const { error, data: validInput } = issueFormInputValidation.safeParse(input);
+  if (error) {
+    return NextResponse.json({ error: error.format() }, { status: 400 });
+  }
+
+  const oldIssue = await prisma.issue.findUnique({
+    where: { id: validId },
+  });
+  if (oldIssue === null) {
+    return NextResponse.json(
+      { error: "The requested resources cannot be found" },
+      { status: 404 }
+    );
+  }
+
+  const updatedIssue = await prisma.issue.delete({
+    where: { id: validId },
+  });
+
+  const response = { data: updatedIssue };
+  return NextResponse.json(response, {
+    status: 200,
   });
 }
