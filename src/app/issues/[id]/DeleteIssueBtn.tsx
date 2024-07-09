d9266270f4e07a58ccea33a6d4d23a28db345975
ucok-man <ucokkocu411@gmail.com>
2024-07-09 20:40:21 +0700

    add alet dialog when delete issue

--- src/app/issues/[id]/DeleteIssueBtn.tsx
+++ src/app/issues/[id]/DeleteIssueBtn.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 type Props = {
   issueId: number;
@@ -7,8 +6,28 @@ type Props = {
 
 export default function DeleteIssueBtn({ issueId }: Props) {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure want to delete this issue? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Continue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
--- src/app/issues/[id]/IssueDetail.tsx
+++ src/app/issues/[id]/IssueDetail.tsx
@@ -10,14 +10,16 @@ type Props = {
 export default function IssueDetail({ issue }: Props) {
   return (
     <>
-      <Heading>{issue.title}</Heading>
+      <Heading as="h1" size="7">
+        {issue.title}
+      </Heading>
       <Flex align="center" gap="3" my="2">
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
       <Card className="prose prose-sm max-w-full" mt="5">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>{" "}
+      </Card>
     </>
   );
 }
