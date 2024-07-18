e389d6204edaa3114254bbbbadb5ba98d762bc0a
ucok-man <ucokkocu411@gmail.com>
2024-07-18 18:05:19 +0700

    add edit comment ui

--- src/app/issues/[id]/(id)/CommentDisplay.tsx
+++ src/app/issues/[id]/(id)/CommentDisplay.tsx
@@ -1,6 +1,16 @@
+import { auth } from "@/auth";
 import prisma from "@db/client";
 import { Comment, User } from "@prisma/client";
-import { Avatar, Box, Card, Flex, Separator, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Separator,
+  Text,
+} from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
 
 type Props = {
@@ -35,27 +45,40 @@ type CommentDisplayCardProps = {
   author: User;
 };
 
-function CommentDisplayCard({ comment, author }: CommentDisplayCardProps) {
+async function CommentDisplayCard({
+  comment,
+  author,
+}: CommentDisplayCardProps) {
+  const session = await auth();
+
   return (
     <>
       <Card>
-        <Flex gap="3" align="center" mb={"2"}>
-          <Avatar size="3" src={author.image!} radius="full" fallback="?" />
-          <Box>
-            <Text as="div" size="2" weight="bold">
-              {author.name}
-            </Text>
-            <Text as="div" size="1" color="gray">
-              commented on {comment.createdAt.toDateString()}
-            </Text>
-          </Box>
+        <Flex justify={"between"} align={"center"}>
+          <Flex gap="3" align="center" mb={"2"}>
+            <Avatar size="3" src={author.image!} radius="full" fallback="?" />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                {author.name}
+              </Text>
+              <Text as="div" size="1" color="gray">
+                commented on {comment.createdAt.toDateString()}
+              </Text>
+            </Box>
+          </Flex>
+          {session?.user?.id === author.id && (
+            <Button className="cursor-pointer text-sm">
+              <Pencil2Icon />
+              Edit
+            </Button>
+          )}
         </Flex>
         <Card className="prose prose-headings:text-2xl max-w-full" mt="1">
           <ReactMarkdown>{comment.description}</ReactMarkdown>
         </Card>
       </Card>
       <Box px={"6"}>
-        <Separator orientation={"vertical"} size={"2"} />
+        <Separator orientation={"vertical"} size={"1"} />
       </Box>
     </>
   );
