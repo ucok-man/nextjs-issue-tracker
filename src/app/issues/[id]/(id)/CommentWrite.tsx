ac0a194a80b16d41f11e00d751df6d0bea73f280
ucok-man <ucokkocu411@gmail.com>
2024-07-18 16:52:46 +0700

    change user image when commenting

--- src/app/issues/[id]/(id)/CommentWrite.tsx
+++ src/app/issues/[id]/(id)/CommentWrite.tsx
@@ -5,6 +5,7 @@ import { createCommentDTO } from "@/types/commentDTO";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Avatar, Button, Card, Flex, Spinner, Text } from "@radix-ui/themes";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -18,6 +19,7 @@ type Props = {
 };
 
 export default function CommentWrite({ issueId }: Props) {
+  const { data: session } = useSession();
   const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const {
@@ -57,7 +59,7 @@ export default function CommentWrite({ issueId }: Props) {
       <Flex gap="3" align="center" mb={"2"}>
         <Avatar
           size="3"
-          src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+          src={session?.user?.image!}
           radius="full"
           fallback="?"
         />
